bbe54436461dc29798e4d3c59342dbc57ed3bb3d
heathersmith <moellio.heather@gmail.com>
2022-09-13 07:54:26 -0400

    adds urls paths, http://127.0.0.1:8000/snippets/ producing errors

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer
-
-# Create your views here.
 
 
 class SnippetList(generics.ListCreateAPIView):
