76a643c2641ecdb7cba11b173621cc3340b47436
heathersmith <moellio.heather@gmail.com>
2022-09-13 13:58:07 -0400

    finished. url was working inconsistently due to incorrect pipenv'd files to pipfile

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',  # new
-    'snippets',  # new
+    'rest_framework',
+    'snippets',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# tutorial/settings.py
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path  # new
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('snippets.urls')),  # new
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('snippets.urls')),
 ]
